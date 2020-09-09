862cdda10d81f821a1937079263929f3aeba5f44
snag <somjitnag192@gmail.com>
2020-09-09 21:06:07 +0530

    Bean Lifecycle basics PostConstruct and PreDestroy

--- src/main/java/com/in28minutes/spring/basics/springin5steps/PersonDao.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/PersonDao.java
@@ -2,13 +2,27 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Getter @Setter
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Getter @Setter @Log4j2
 @Component
 public class PersonDao {
     @Autowired
     JdbcConnection jdbcConnection;
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info(">> This is logged just after PersonDao is created. #beanlifecyle");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info(">> This is logged just before PersonDao is removed. #beanlifecyle");
+    }
 }
